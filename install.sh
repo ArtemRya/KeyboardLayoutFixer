ccaf5dccbd417e619c66ca8d5a3979a7fe9e2cc4
Artem Ryabov <artemr@quantbox.ru>
2025-12-11 18:27:25 +0300

    remove useless install.sh

--- install.sh
+++
@@ -1,9 +0,0 @@
-# Скачайте файлы
-wget https://raw.githubusercontent.com/your-repo/layout_fixer.py
-wget https://raw.githubusercontent.com/your-repo/install.sh
-
-# Сделайте исполняемыми
-chmod +x layout_fixer.py install.sh
-
-# Запустите установку
-./install.sh
